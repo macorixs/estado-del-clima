4e9d54e9d209ebce16d884ce398aa3f4b7409d7b
macorixs <147300832+macorixs@users.noreply.github.com>
2023-10-18 12:26:12 -0400

    Add files via upload

---
+++ satelite.js
@@ -0,0 +1,219 @@
+function satelite(){
+      var satelite = document.getElementById('satelite')
+    console.log('satelite');
+    if('satelite'){
+     console.log('true');
+     // true // 
+     console.log('false');
+     // false ///
+    }
+  
+
+function iniciar_satelite(){
+
+     var iniciarsatelite = document.getElementById('ininiar_satelite')
+
+console.log("iniciar_satelite");
+if('iniciar_satelite'){
+     console.log('true');
+     // true // 
+     console.log('false');
+     // false //
+}
+
+
+}
+
+
+function blue(){ 
+     var blue = document.getElementById('blue')
+     var blue = "azul";
+     console.log('blue');
+     if('blue'){
+          console.log('true');
+          // true //
+          console.log('false');
+          // false //
+     }
+  
+}
+
+
+function visible(){
+     var visible = document.getElementById('visible')
+     var visible = "amarillo";
+     console.log('visible');
+     if('visible'){
+          console.log('true');
+          // true //
+          console.log('false')
+     }
+}
+
+function infrarojos(){
+      var infrarojos = document.getElementById('infrarojos')
+      var infrarojos = "rojos";
+      console.log('infrarojos');
+      if('infrarojos'){
+          console.log('true');
+          // true //
+          console.log('false');
+          // false //
+      }
+
+}
+// paises mundial//
+// paises y cuidades//
+var url = `https://api.openweathermap.org/data/2.5/weather?q=santo domingo&lang=es&units=metric&appid=${'2b1bbf3596fedcae5e9eea8f74582b54'}`
+
+function conexionviasatelite(conexionviasatelite){
+     var conexionviasatelite = document.getElementById('conexion via satelite') 
+     var conexionpuertousb = document.getElementById('conexion puerto usb')
+     console.log('conexionviasatelite');
+     if('res = 500% => 65% <= 100%');{
+          console.log('res');
+     }
+
+       
+}
+
+var url = `https://title.openweathermap.org/map/{layer}/{z}/{x}/{y}.png?appid={'2b1bbf3596fedcae5e9eea8f74582b54'}`;
+
+function estaciones(){
+     var base = "estaciones";
+var estaciones = document.getElementById('estaciones')
+console.log('estaciones');
+if('estaciones'){
+     console.log('true');
+     // true //
+     console.log('false');
+     // false //
+}
+
+
+
+}
+
+function esperando(){
+     var esperando = document.getElementById('esperando')
+     var esperando = "animaciones";
+  console.log('esperando');
+}
+
+
+function ubicacion(){ 
+     var satelite = "ubicacion";
+     var satelite = document.getElementById('ubicacion')
+     var localizacion = document.getElementById('nubes')
+var localizacion = "nubes";
+
+console.log('ubicacion');
+if('ubicacion'){
+     console.log('true');
+     // true //
+
+     console.log('false');
+     // false //
+}
+
+
+
+
+}
+
+function animaciones(){
+     var animaciones = document.getElementById('animaciones')
+     var animaciones = "imagenes del satelite";
+  console.log('animaciones');
+  if('animaciones'){
+     console.log('true');
+     // true // 
+     console.log('false');
+     // false //
+  }
+}
+
+
+function conexionesalradar(satelital){
+    var radar1 = document.getElementById('radar1')
+    var radar1 = "imagenes del satelite";
+    var radar2 = document.getElementById('radar2')
+    var radar2 = "imagenes del satelite";
+console.log('conexionesalradar');
+if('conexionesalradar'){
+     console.log('true');
+     // true // 
+     console.log('false');
+     // false //
+}
+
+}
+
+
+function datos(satelital){
+     var datos = document.getElementById('datos')
+     var mostrardatos = document.getElementById('mostrar datos')
+     var mostrardatosdelsatelite = document.getElementById('mostrar datos del satelite')
+     if('datos'){
+          console.log('true');
+          // true // 
+          console.log('false');
+          // false //
+     }
+
+}
+
+function localizandonubes(satelital){
+      var localizandonubes = document.getElementById('localizando nubes')
+      var localizandonubes = "imagenes satelitales";
+  console.log('localizandonubes');
+  if('localizando la nubes');{
+     console.log('true');
+     // true // 
+     console.log('false');
+     // false // 
+  }
+}
+
+
+function visualizaciondelacamara(satelital){
+      var visualizacionconlacamara = document.getElementById('visualizacion con la camara')
+      var visualizacionconlacamara = "imagenes satelitales";
+  console.log('visualizaciondelacamara');
+  if('visualisacion con la camara');{
+     console.log('true');
+     // true //
+     console.log('false')
+     // false //
+  }
+}
+
+
+function imagenesdelsatelite(){
+      var imagenesdelsatelite = document.getElementById('imagenes del satelite')
+      var imagenesdelsatelite = "imagenes del satelite";
+  console.log('imagenes del satelite');
+  if('imagenes del satelite');{
+     console.log('true');
+     // true // 
+     console.log('false');
+     // false
+  }
+}
+
+window.onload =  function nubes(){
+     var nubes = document.getElementById('nubes')
+     nubes.addEventListener('change')
+          HTML.animaciones('nubes');
+
+     
+}
+
+window.onload = function cargarnubes(animaciones){
+     var cargarnubes = document.getelementbyld( 'cargar nubes' )
+     console.log( 'cargarnubes' ) ; 
+     
+     }
+
+
+}
